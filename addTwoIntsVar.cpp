9f8dfec83f632a126b0d763e880fd91b66c3554e
Anne DeWulf <anneldewulf@gmail.com>
2024-08-21 11:06:15 -0700

    added addTwoIntsVar.cpp
    Second assignment regarding variables in class today (8/21/2024)

---
+++ addTwoIntsVar.cpp
@@ -0,0 +1,30 @@
+// Anne DeWulf 8/21/24
+// sumTwoIntsVar.cpp
+// Made in 5 minutes
+
+#include <thread>
+#include "iostream"
+using namespace std;
+
+int userInt1 = 0;
+int userInt2 = 0;
+int endInt = 0;
+
+void getInts() {
+    cout << "Please enter an number: ";
+    cin >> userInt1;
+
+    cout << "Please enter another number: ";
+    cin >> userInt2;
+
+    endInt = userInt1 + userInt2;
+}
+
+int main() {
+
+    getInts();
+
+    cout << "The value is: " << endInt << endl;
+
+    return 0;
+}
